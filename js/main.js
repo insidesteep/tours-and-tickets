d767d7ba603d23f0f9df5666c601aaa942038595
Suxrob <salaxiddinovsuxrob@gmail.com>
2022-07-04 18:01:28 +0500

    ww

--- js/main.js
+++ js/main.js
@@ -1,96 +1,150 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  // select language
+
+  const select = document.querySelector(".selectpicker");
+  const options = select.options;
+  const path = window.location.pathname;
+
+  for (let i = 0; i < options.length; i++) {
+    const option = options[i];
+
+    console.log(option.value, path);
+
+    if (option.value == path) {
+      option.selected = true;
+    } else {
+      option.select = false;
+    }
+  }
+
+  // $('.selectpicke').selectpicker();
+
+  const feedbackForm = document.querySelector("#feedback");
+  const name = feedbackForm.querySelector("input#name");
+  const email = feedbackForm.querySelector("input#email");
+  const phone = feedbackForm.querySelector("input#phone");
+  const text = feedbackForm.querySelector("textarea#message");
+  const submitButton = feedbackForm.querySelector("button#submit");
+  const loader = document.querySelector("#spinner");
+  const loaderText = loader.querySelector(".sr-only");
+
+  submitButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const formdata = new FormData();
+
+    formdata.append("name", name.value);
+    formdata.append("email", email.value);
+    formdata.append("text", text.value);
+    formdata.append("phone", phone.value);
+
+    var requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
     };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
 
+    loader.classList.add("show");
+    loaderText.innerText = "Отправка данных...";
+    loaderText.classList.remove("sr-only");
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
-    
-    
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    const response = await fetch(
+      "https://qayerda.uz/feedback.php",
+      requestOptions
+    );
 
+    const data = await response.json();
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
+    alert(data.message);
 
-    // $('.selectpicke').selectpicker();
-    
+    loader.classList.remove("show");
+    loaderText.innerText = "";
+    loaderText.classList.add("sr-only");
+  });
 })(jQuery);
-
